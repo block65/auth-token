95ebafd5a56a9d6a0830ece90f94d71518f04a16
Max Holman <max@holmn.com>
2022-12-14 21:00:58 +0800

    refactor: boilerplate update

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+export default {
+  modulePathIgnorePatterns: ['<rootDir>/dist'],
+  testMatch: ['<rootDir>/__tests__/**/*.test.[jt]s?(x)'],
+  moduleNameMapper: {
+    '^(\\..*)\\.jsx?$': '$1', // support for ts imports with .js extensions
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
 };
